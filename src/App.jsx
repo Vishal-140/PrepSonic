2fd0b5aa79c66ef17641673207d1ca154324a79a
Vishal Kumar Chaurasia <vkc140@gmail.com>
2025-06-25 00:25:26 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-600">
+        Hello PrepSonic!
+      </h1>
+    </div>
+  )
+}
+
+export default App;
